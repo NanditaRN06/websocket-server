87a2017628530e7f1bfe12722a0566f925f6b990
Nandita R Nadig <152580855+NanditaRN06@users.noreply.github.com>
2026-02-02 15:11:10 +0530

    Updated server.py

--- server.py
+++ server.py
@@ -2,23 +2,30 @@ from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 
 app = FastAPI()
 
-clients = set()
+devices = {}
+
+@app.get("/")
+def health():
+    return {"status": "WebSocket server running"}
 
 @app.websocket("/ws")
 async def websocket_endpoint(ws: WebSocket):
     await ws.accept()
-    clients.add(ws)
-    print("Client connected")
 
     try:
-        while True:
-            msg = await ws.receive_text()
-            print("Received:", msg)
+        # First message = registration
+        data = await ws.receive_json()
+        device_id = data["device_id"]
+        devices[device_id] = ws
+        print(f"{device_id} connected")
 
-            for client in clients:
-                if client != ws:
-                    await client.send_text(msg)
+        while True:
+            msg = await ws.receive_json()
+            target = msg["to"]
+
+            if target in devices:
+                await devices[target].send_json(msg)
 
     except WebSocketDisconnect:
-        clients.remove(ws)
-        print("Client disconnected")
+        devices.pop(device_id, None)
+        print(f"{device_id} disconnected")
